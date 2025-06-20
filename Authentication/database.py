3b4d7647d57ec71ad5f086011332b1bb442d6043
ndithomas <ndithomas30@gmail.com>
2025-06-20 09:23:51 +0200

    adjusted env file  on Authentication

--- Authentication/database.py
+++ Authentication/database.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = quote_plus(os.getenv("MYSQL_PASSWORD", "Tomy@28728283701920"))
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_DB = os.getenv("MYSQL_DB", "AuthProductdb")
+MYSQL_PASSWORD = quote_plus(os.getenv("MYSQL_PASSWORD"))
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DB = os.getenv("MYSQL_DB")
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
 engine = create_engine(DATABASE_URL, echo=True)
